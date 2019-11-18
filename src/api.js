59033bf77a818b8cd6b83742c2e8a384a982ba95
Nico Virgino <nicovirgino@gmail.com>
2019-11-17 19:43:33 -0600

    installed omdb api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 module.exports = {
   getMovies: () => {
-    return fetch('/api/movies')
+    return fetch(`/api/movies/`)
       .then(response => response.json());
   },
+
   getMovie: (id) => {
     return fetch(`/api/movies/${id}`)
         .then(response => response.json())
@@ -18,26 +19,22 @@ module.exports = {
     })
 
   },
-  patchMovie : (movie, id) => {
-    return fetch(`api/movies/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(movie),
-    })
-  },
-  deleteMovie : (id) => {
-    return fetch(`api/movies/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-  },
-  getMovieKey : () => {
-    return fetch('http://www.omdbapi.com/?apikey=ffbdb1fc&')
-        .then(response => response.json())
-  }
+  // patchMovie : (movie, id) => {
+  //   return fetch(`api/movies/${id}`, {
+  //     method: 'PATCH',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(movie),
+  //   })
+  // },
+  // deleteMovie : (id) => {
+  //   return fetch(`api/movies/${id}`, {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  // }
 
 };
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@
  */
 // import sayHello from './hello';
 // sayHello('World');
-
-
 // import {getMovies} from './api.js';
 
 
 /**
  * require style imports
  */
+// const omdb = new (require(' omdbapi '))('ffbdb1fc');
 
 const $ = require('jquery');
+
 import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.js'
 import 'materialize-css/js/sidenav.js'
@@ -21,10 +21,11 @@ import M from 'materialize-css/dist/js/materialize.js'
 ///////////API KEY///////////////////////////////////
 // const key = 'http://www.omdbapi.com/?apikey=782d1187&t=';
 
-const {getMovieKey, getMovie, getMovies, createMovie, patchMovie, deleteMovie} = require('./api.js');
+const {getMovie, getMovies, createMovie, patchMovie, deleteMovie} = require('./api.js');
 
 var elems = document.querySelectorAll('.sidenav');
 var instances = M.Sidenav.init(elems);
+
 
 // $(document).ready(function () {
 // $('.sidenav').sidenav();
@@ -37,137 +38,143 @@ $(document).ready(function () {
 });
 
 
-// renderMovies()
-//     .then((movie) => {
-//         let html = '<div class="coffee-card card d-flex align-items-center border-0 m-2">';
-//         html += '<div class="card-body m-3 w-100 d-flex justify-content-center "><img src="img/bean.png" height="50px" alt="">' + '</div>';
-//         html += '<span>' + movie.title + '</span>';
-//         html += '<span>' + movie.rating + '</span>';
-//         html += '</div>';
-//         return html;
+//////////////////////////////
+//////search through api//////
+/////////////////////////////
+// omdb.search('saw', function(err, movies) {
+//     if (err) {
+//         return console.error(err);
+//     }
+//
+//     if (movies.length < 1) {
+//         return console.log('No movies were found!');
+//     }
+//
+//     movies.forEach(function (movie) {
+//         console.log('%s (%d)', movie.title, movie.year);
 //     });
 
 ////////////////////////
 //////GET MOVIES////////
 ////////////////////////
 
-getMovies()
-    .then((movies) => {
-        console.log('Here are all the movies:');
-        movies.forEach(({title, rating, id}) => {
-            console.log(`id#${id} - ${title} - rating: ${rating}`);
-        });
-    }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
+// getMovies()
+//     .then((movies) => {
+//         console.log('Here are all the movies:');
+//         movies.forEach(({title, rating, id}) => {
+//             console.log(`id#${id} - ${title} - rating: ${rating}`);
+//         });
+//     }).catch((error) => {
+//     alert('Oh no! Something went wrong.\nCheck the console for details.');
+//     console.log(error);
+// });
 
 
 ////////////////////////
 //////GET ONE MOVIE/////
 ////////////////////////
 
-getMovie(1)
-    .then((movie) => {
-        console.log('Here is the first movie: ');
-        console.log(`id#${movie.id} - ${movie.title} - rating: ${movie.rating}`);
-
-    })
-    .catch((error) => {
-        alert('Oh no! Something went wrong.\nCheck the console for details.');
-        console.log(error);
-    });
+// getMovie(1)
+//     .then((movie) => {
+//         console.log('Here is the first movie: ');
+//         console.log(`id#${movie.id} - ${movie.title} - rating: ${movie.rating}`);
+//
+//     })
+//     .catch((error) => {
+//         alert('Oh no! Something went wrong.\nCheck the console for details.');
+//         console.log(error);
+//     });
 
 ///////////////////////
 //////CREATE MOVIE/////
 ///////////////////////
 
-createMovie({
-
-}).then(getMovies).then((movies) => {
-    // console.log('Here are all the movies: ' + movies);
-    movies.forEach(({title, rating}) => {
-        console.log(`title:${title} - rating: ${rating}`);
-    });
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
+// createMovie({
+//
+// }).then(getMovies).then((movies) => {
+//     // console.log('Here are all the movies: ' + movies);
+//     movies.forEach(({title, rating}) => {
+//         console.log(`title:${title} - rating: ${rating}`);
+//     });
+// }).catch((error) => {
+//     alert('Oh no! Something went wrong.\nCheck the console for details.');
+//     console.log(error);
+// });
 
 
 ///////////////////////
 //////PATCH MOVIE/////
 ///////////////////////
 
-patchMovie({
-    "title": "Darjeeling Limited",
-    "rating": "5"
-}, 3).then(getMovies).then((movies) => {
-    console.log('Here are all the movies:');
-    movies.forEach(({title, rating}) => {
-        console.log(`${title}, ${rating}`);
-    });
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
+// patchMovie({
+//     "title": "Darjeeling Limited",
+//     "rating": "5"
+// }, 3).then(getMovies).then((movies) => {
+//     console.log('Here are all the movies:');
+//     movies.forEach(({title, rating}) => {
+//         console.log(`${title}, ${rating}`);
+//     });
+// }).catch((error) => {
+//     alert('Oh no! Something went wrong.\nCheck the console for details.');
+//     console.log(error);
+// });
 
 ///////////////////////
 //////DELETE MOVIE/////
 ///////////////////////
 
 
-deleteMovie(4).then(getMovies).then((movies) => {
-    console.log('Here are all the books:');
-    movies.forEach(({title, rating}) => {
-        console.log(`${title} ${rating}`);
-    });
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
+// deleteMovie(4).then(getMovies).then((movies) => {
+//     console.log('Here are all the books:');
+//     movies.forEach(({title, rating}) => {
+//         console.log(`${title} ${rating}`);
+//     });
+// }).catch((error) => {
+//     alert('Oh no! Something went wrong.\nCheck the console for details.');
+//     console.log(error);
+// });
 
 
-getMovieKey()
-    .then((movies) => {
-        console.log('Here are all the movies:');
-        movies.forEach(({title, rating, id}) => {
-            console.log(`id#${id} - ${title} - rating: ${rating}`);
-        });
-    }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
+// getMovieKey()
+//     .then((movies) => {
+//         console.log('Here are all the movies:');
+//         movies.forEach(({title, rating, id}) => {
+//             console.log(`id#${id} - ${title} - rating: ${rating}`);
+//         });
+//     }).catch((error) => {
+//     alert('Oh no! Something went wrong.\nCheck the console for details.');
+//     console.log(error);
+// });
 
 /////////////////////////////////
 ////////SEARCH FEATURE//////////
 ///////////////////////////////
-$(document).ready(function () {
-    $('#search-button').click(function () {
-        let input = $("#search").val();
-        $('#search').html("");
-        fetch(key + input)
-            .then((success) => {
-                success.json()
-            })
-            .then((movies) => {
-                console.log(movies);
-                let content = "<div class='card'>";
-                content += '<img id="logo" src= "'+ movies.poster + '" alt="">';
-                content += movies.title;
-                content += '<br>';
-                content += movies.released;
-                content += '<br>';
-                content += movies.plot;
-                content += "</div>";
-
-                $('#card').append(content);
-            })
-            .catch((error) => {
-                console.log(error)
-            });
-    });
-});
+// $(document).ready(function () {
+//     $('#search-button').click(function () {
+//         let input = $("#search").val();
+//         $('#search').html("");
+//         fetch(omdb + input)
+//             .then((success) => {
+//                 success.json()
+//             })
+//             .then((movies) => {
+//                 console.log(movies);
+//                 let content = "<div class='card'>";
+//                 content += '<img id="logo" src= "'+ movies.poster + '" alt="">';
+//                 content += movies.title;
+//                 content += '<br>';
+//                 content += movies.released;
+//                 content += '<br>';
+//                 content += movies.plot;
+//                 content += "</div>";
+//
+//                 $('#card').append(content);
+//             })
+//             .catch((error) => {
+//                 console.log(error)
+//             });
+//     });
+// });
 
 
 
