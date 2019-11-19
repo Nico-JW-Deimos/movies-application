eabdc8c5edae00975faa2780c982d74366c03254
Nico Virgino <nicovirgino@gmail.com>
2019-11-18 23:39:25 -0600

    got on load, add new card to page

--- src/api.js
+++ src/api.js
@@ -3,12 +3,12 @@ module.exports = {
     return fetch(`/api/movies/`)
       .then(response => response.json());
   },
-
-  // getMovie: (id) => {
-  //   return fetch(`/api/movies/${id}`)
-  //       .then(response => response.json())
-  // },
-
+//
+//   // getMovie: (id) => {
+//   //   return fetch(`/api/movies/${id}`)
+//   //       .then(response => response.json())
+//   // },
+//
   createMovie: (movie) => {
     return fetch(`/api/movies/`, {
       method: 'POST',
@@ -18,22 +18,22 @@ module.exports = {
       body: JSON.stringify(movie),
     })
   },
-  // patchMovie : (movie, id) => {
-  //   return fetch(`api/movies/${id}`, {
-  //     method: 'PATCH',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(movie),
-  //   })
-  // },
-  deleteMovie : (id) => {
-    return fetch(`api/movies/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-  }
-
+//   patchMovie : (movie, id) => {
+//     return fetch(`api/movies/${id}`, {
+//       method: 'PATCH',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(movie),
+//     })
+//   },
+//   deleteMovie : (id) => {
+//     return fetch(`api/movies/${id}`, {
+//       method: 'DELETE',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     })
+//   }
+//
 };
--- src/index.js
+++ src/index.js
@@ -27,24 +27,43 @@ const $ = require('jquery');
 
 const {getMovie, getMovies, createMovie, patchMovie, deleteMovie} = require('./api.js');
 
-// var elems = document.querySelectorAll('.sidenav');
-// var instances = M.Sidenav.init(elems);
-
 
 ////////////////////////
 //////GET MOVIES////////
 ////////////////////////
 
+// getMovies()
+//     .then((movies) => {
+//         console.log('Here are all the movies:');
+//         movies.forEach(({title, rating, id}) => {
+//             console.log(`id#${id} - ${title} - rating: ${rating}`);
+//         });
+//     }).catch((error) => {
+//     alert('Oh no! Something went wrong.\nCheck the console for details.');
+//     console.log(error);
+// });
+
 getMovies()
     .then((movies) => {
-        console.log('Here are all the movies:');
-        movies.forEach(({title, rating, id}) => {
-            console.log(`id#${id} - ${title} - rating: ${rating}`);
-        });
-    }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
+        let showMovies = (movies) => {
+            $('#theLoader').addClass("invisible");
+            movies.forEach(({title, rating, image}) => {
+                $('#card-container').append(`
+                <div class="card">
+                <h6 id="movieTitle">${title}</h6>
+                <img src="img/${image}" alt="">
+                <p>${rating}️️</p>
+                <div class="buttons">
+                <button class="gear">⚙️</button>
+                <button class="trash">🗑</button>
+                </div>
+                </div>`);
+
+            });
+
+        };
+        showMovies(movies);
+    });
 
 
 ////////////////////////
@@ -65,35 +84,58 @@ getMovies()
 ///////////////////////
 //////CREATE MOVIE/////
 ///////////////////////
-$('.search-button').click(function(){
-    let title = $('.title').val();
-createMovie({
+function loadMovies() {
 
-    'title': `${title}`,
-    'rating': 100
-})
 
-    .then(getMovies)
-    .then((movies) => {
-        console.log('Here are all the movies: ' + movies);
-        movies.forEach(({title, rating, id}) => {
-            console.log(`title:${title} - rating: ${rating} - id ${id}`);
+    let title = $('#title').click(function () {
+
+        createMovie({
+
+            'title': title.val(),
+
+        })
+
+            .then(getMovies)
+            .then((movies) => {
+                console.log('Here are all the movies: ' + movies);
+                movies.forEach(({title, rating, id}) => {
+                    console.log(`title:${title} - rating: ${rating} - id ${id}`);
+                });
+            }).catch((error) => {
+            alert('Oh no! Something went wrong.\nCheck the console for details.');
+            console.log(error);
+
+
         });
-    }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
+    });
+
+}
+loadMovies();
+//Click listener for the submit button
+// const addSubmitBtnListener = function () {
+//     $('#submit').click(function () {
+//         //Create new movie object
+//         let newMovie = {
+//             title: $('#movie-name').val(),
+//             rating: $('#movie-rating').val(),
+//             genre: $('#movie-genre').val()
+//         };
+//         //Add the movie to the db
+//         addMovie(newMovie).then(() => {
+//             //Dynamically reload the page
+//             loadMovies();
+//         });
+//     });
+// };
 
 
-
-});
 ///////////////////////////
 //////CREATE MOVIE BTN/////
 //////////////////////////
 
 //Click listener for the submit button
 // const addSubmitBtnListener = () => {
-//     $('#submit').click(function () {
+//     $('#title').click(function () {
 //         //Create new movie object
 //         let newMovie = {
 //             title: $('#movie-name').val(),
@@ -101,59 +143,49 @@ createMovie({
 //         };
 //         console.log(newMovie);
 //         //Add the movie to the db
-//         createMovie((newMovie)
-//             .then(() => {
-//             }));
+//         getMovies().then((movies) => {
+//             //append stuff here:
+//             $('.card-container').append("");
+//         });
+//
 //     });
-// // };
-// addSubmitBtnListener();
-// $('#search-button').click(function () {
-//     let newMovie = $('#search').val();
-//     console.log(createMovie(newMovie));
-// $('#card-container').append(`<div class="card">
-//     <h6>${title}</h6>
-//     <p>${rating}️️</p>
-//     <div class="buttons">
-//         <button class="gear">⚙️</button>
-//         <button class="trash">🗑</button>
-//     </div>
-// </div>`)
-// });
+//
+// };
 
 
 ///////////////////////
 //////PATCH MOVIE/////
 ///////////////////////
-
-// patchMovie({
-//     "title": "Darjeeling Limited",
-//     "rating": "5"
-// }, 3).then(getMovies).then((movies) => {
-//     console.log('Here are all the movies:');
-//     movies.forEach(({title, rating}) => {
-//         console.log(`${title}, ${rating}`);
-//     });
-// }).catch((error) => {
-//     alert('Oh no! Something went wrong.\nCheck the console for details.');
-//     console.log(error);
-// });
+//
+//         patchMovie({
+//             "title": "Darjeeling Limited",
+//             "rating": "5"
+//         }, 3).then(getMovies).then((movies) => {
+//             console.log('Here are all the movies:');
+//             movies.forEach(({title, rating}) => {
+//                 console.log(`${title}, ${rating}`);
+//             });
+//         }).catch((error) => {
+//             alert('Oh no! Something went wrong.\nCheck the console for details.');
+//             console.log(error);
+//         });
 
 ///////////////////////
 //////DELETE MOVIE/////
 ///////////////////////
 
 
-deleteMovie().then(getMovies).then((movies) => {
-    console.log('Here are all the movies:');
-    movies.forEach(({title, rating}) => {
-        console.log(`${title} ${rating}`);
-    });
-    addDeleteBtnListener();
-
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
+// deleteMovie().then(getMovies).then((movies) => {
+//     console.log('Here are all the movies:');
+//     movies.forEach(({title, rating}) => {
+//         console.log(`${title} ${rating}`);
+//     });
+//     addDeleteBtnListener();
+//
+// }).catch((error) => {
+//     alert('Oh no! Something went wrong.\nCheck the console for details.');
+//     console.log(error);
+// });
 
 
 /////////////////////////////////
@@ -191,13 +223,13 @@ deleteMovie().then(getMovies).then((movies) => {
 ////////DELETE FEATURE//////////
 ///////////////////////////////
 
-const addDeleteBtnListener = function() {
-    $('.trash').click(function () {
-        $('.cardOne').hide();
-        deleteMovie(parseInt($(this).attr('id')))
-    });
-
-};
+// const addDeleteBtnListener = function() {
+//     $('.trash').click(function () {
+//         $('.cardOne').hide();
+//         deleteMovie(parseInt($(this).attr('id')))
+//     });
+//
+// };
 
 
 
